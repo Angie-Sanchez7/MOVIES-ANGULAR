7f0a3383820da77548d3f00b3ff2612eff7a6c0b
Zayda Rivera <zayda07@misena.edu.co>
2021-09-27 00:18:53 -0500

    se añade documento proxy, se modifica angular.json, package.json, app.module y home.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AddComponent } from './components/add/add.component';
 import { EditComponent } from './components/edit/edit.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AddComponent,
-    EditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AddComponent, EditComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipoService } from '../../services/equipo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private EquipoService: EquipoService) {}
 
   ngOnInit(): void {
+    this.listarEquipo();
   }
 
+  listarEquipo() {
+    this.EquipoService.getEquipos().subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => console.log(err)
+    );
+  }
 }
